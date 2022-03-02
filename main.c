e06d853529bfe7304e074d743c83920ca233d6a0
Mugendi01 <99478755+Mugendi01@users.noreply.github.com>
2022-03-02 21:32:09 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+/*
+COUNTY LIBRARY MANAGEMENT SYSTEM
+By Ireri M Mugendi
+C89 compiler
+Feb 2022
+MIT License
+C89 compiler
+*/
+  #include <stdio.h>
+  #include <stdio.h>
+int main()
+{
+    int action;
+
+    printf("select an action below\n");
+    printf("1. add new patron\n")
+    printf("3.view all patrons\n");
+    printf("3.view all books \n");
+    printf("4. add a new book\n");
+    printf("your action:\n");
+        scanf("%d",& action);
+
+             return action;
+}
+int main (){
+
+         printf("COUNTY LIBRARY SYSTEM\n");
+         printf("WELCOME MR .Mugendi\n");
+         printf("your selected action %d"Menu:\n");
+
+         retur 0;
+}
+
+
+
+
+
+}
